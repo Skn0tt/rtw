4025cb3edd4abfce0e8d56822fa25a9d9aee566b
Simon Knott <info@simonknott.de>
2021-03-21 12:12:41 +0100

    Fix test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,6 +1,6 @@
 import { makeInput, makeTemplate } from "../src";
 
-test("Easy Mapping", () => {
+test("Only the most current value is passed down", () => {
   const input = makeInput<number, [number]>((send) => (startFrom: number) => {
     send(startFrom);
     send(startFrom + 1);
@@ -14,5 +14,29 @@ test("Easy Mapping", () => {
   const result: number[] = [];
   double.subscribe([3], (v) => result.push(v));
 
-  expect(result).toEqual([6, 8, 10]);
+  expect(result).toEqual([10]);
+});
+
+test("simple mapping", (done) => {
+  const input = makeInput<number, [number]>((send) => (startFrom: number) => {
+    send(startFrom);
+    setTimeout(() => {
+      send(startFrom + 1);
+    }, 1);
+    setTimeout(() => {
+      send(startFrom + 2);
+    }, 2);
+  });
+
+  const double = makeTemplate((startFrom: number) => {
+    return input(startFrom) * 2;
+  }, "double");
+
+  const result: number[] = [];
+  double.subscribe([3], (v) => result.push(v));
+
+  setTimeout(() => {
+    expect(result).toEqual([6, 8, 10]);
+    done();
+  }, 5);
 });
