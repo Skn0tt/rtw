e901ddb4b291ffbc0463b264dc418185dbe4e03c
Simon Knott <info@simonknott.de>
2021-03-22 14:50:36 +0100

    add test about delayed multi-send

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -21,6 +21,34 @@ test("Only the most current value is passed down", () => {
   expect(result).toEqual([10]);
 });
 
+test("Multiple synchronous sends trigger multiple synchronous evaluations", (done) => {
+  const input = makeLiveValue<number, [number]>(
+    (startFrom: number) => (send) => {
+      const timer = setTimeout(() => {
+        send(startFrom);
+        send(startFrom + 1);
+        send(startFrom + 2);
+      }, 1);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  );
+
+  const double = makeDerivedValue((startFrom: number) => () => {
+    return input(startFrom) * 2;
+  });
+
+  const result: number[] = [];
+  double.subscribe([3], (v) => result.push(v));
+
+  setTimeout(() => {
+    expect(result).toEqual([6, 8, 10]);
+    done();
+  }, 5);
+});
+
 test("simple mapping", (done) => {
   const input = makeLiveValue<number, [number]>(
     (startFrom: number) => (send) => {
